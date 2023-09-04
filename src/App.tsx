62ce7ca3b26518ac9f8f21264cf7ff38a69fa7cb
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2023-09-04 12:48:06 -0700

    React router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.scss'
 import Navbar from './components/Navbar/Navbar.component'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
@@ -9,6 +10,14 @@ function App() {
       <React.Fragment>
         <Navbar />
       </React.Fragment>
+      <div>
+        <Routes>
+          {/* <Route path='/' element={<About />}/>
+          <Route path='/experience' element={<Experience />}/>
+          <Route path='/projects' element={<Projects />}/>
+          <Route path='/contact' element={<Contact />}/> */}
+        </Routes>
+      </div>
     </>
   )
 }
