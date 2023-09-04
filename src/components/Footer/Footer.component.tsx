4223f495d6cd31df18651a64d6493bc724302c8f
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2023-09-04 16:26:45 -0700

    Footer component created

---
+++ src/components/Footer/Footer.component.tsx
@@ -0,0 +1,11 @@
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>Derechos de autor © 2023 Mi Sitio Web</p>
+        </footer>
+    )
+}
+
+export default Footer;
