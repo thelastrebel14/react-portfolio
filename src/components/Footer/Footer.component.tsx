cae0474a6813b41152e4bade727413131ea56c29
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2023-09-04 19:54:17 -0700

    Footer with test social Link

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -1,9 +1,12 @@
+import { FaRebel } from 'react-icons/fa';
+import SocialLink from '../SocialLink/SocialLink.component';
 import './Footer.scss';
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <p>Derechos de autor © 2023 Mi Sitio Web</p>
+        <footer className="footer-container">
+            <SocialLink socialMediaIcon={<FaRebel />} href={'https://colorate.azurewebsites.net/es/Color/878787'} />
+            <p>Andrea Garibaldi</p>
         </footer>
     )
 }
