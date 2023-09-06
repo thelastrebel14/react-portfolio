9f050ed9e2920b421ad93b2d708cb75535b097a7
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2023-09-05 18:16:01 -0700

    code refactor

--- src/components/Navbar/Navbar.component.tsx
+++ src/components/Navbar/Navbar.component.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import { FaBars, FaTimes, FaCode } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import './Navbar.scss';
 
