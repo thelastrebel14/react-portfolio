2aecdb75d6ed6eba9720ab8d557b3f32f050e76d
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2023-09-06 15:25:32 -0700

    Styles refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import './App.scss'
 import Navbar from './components/Navbar/Navbar.component'
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home/Home.component'
+import Experience from './components/Experience/Experience.component'
 import Footer from './components/Footer/Footer.component'
 
 function App() {
-
   return (
     <div className='app'>
       <Navbar />
       <div className='body-container'>
         <Routes>
           <Route path='/react-portfolio' element={<Home />}/>
-          {/* <Route path='/experience' element={<Experience />}/>
-          <Route path='/projects' element={<Projects />}/>
+          <Route path='/experience' element={<Experience />}/>
+          {/* <Route path='/projects' element={<Projects />}/>
           <Route path='/contact' element={<Contact />}/> */}
         </Routes>
       </div>
