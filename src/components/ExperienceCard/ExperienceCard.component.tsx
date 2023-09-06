6bd3463182399fceab4c6c1f7d20c6b3a46703d5
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2023-09-06 16:05:50 -0700

    Mock commit

--- src/components/ExperienceCard/ExperienceCard.component.tsx
+++ src/components/ExperienceCard/ExperienceCard.component.tsx
@@ -7,7 +7,7 @@ const ExperienceCard = () => {
             <h3>August 2021 - July 2023</h3>
         </div>
         <div className='experience-synopsis'>
-            <h3>Experience Title</h3>
+            <h3>Experience Title </h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolorum, minima suscipit nesciunt totam at cupiditate consectetur, eaque, sit est sint perferendis iusto in quisquam culpa repellendus eos aspernatur quod.</p>
             <div className='skill-tags'></div>
         </div>
